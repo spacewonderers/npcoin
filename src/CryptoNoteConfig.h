48ab61d2ae49209bc05bd17ff826fc235013577a
Adzreil <adzreil@gmail.com>
2018-04-06 11:45:07 +0800

    Config update

--- src/CryptoNoteConfig.h
+++ src/CryptoNoteConfig.h
@@ -105,7 +105,7 @@ const char     CRYPTONOTE_BLOCKINDEXES_FILENAME[]            = "blockindexes.bin
 const char     CRYPTONOTE_POOLDATA_FILENAME[]                = "poolstate.bin";
 const char     P2P_NET_DATA_FILENAME[]                       = "p2pstate.bin";
 const char     MINER_CONFIG_FILE_NAME[]                      = "miner_conf.json";
-const char     GENESIS_COINBASE_TX_HEX[]                     = "010a01ff0001ffffffffffff0f029b2e4c0281c0b02e7c53291a94d1d0cbff8883f8024f5142ee494ffbbd08807121013c086a48c15fb637a96991bc6d53caf77068b5ba6eeb3c82357228c49790584a";
+const char     GENESIS_COINBASE_TX_HEX[]                     = "";
 } // parameters
 
 const char     CRYPTONOTE_NAME[]                             = "npcoin";
